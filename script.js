2619d1a30e80299d6688d6f9e0e3de3f27d98d2c
sighmn <simon19@ksu.edu>
2024-10-29 17:06:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,4 +35,34 @@ for(let j = 0; j < 36; j++) {
  }
 }
 
-gridMap();
+
+//temp data for testing
+const records = [
+  { name: "John Doe", location: { township: 6, range: 1, section: 3 } },
+  { name: "John Doe", location: { township: 6, range: 4, section: 5 } },
+  { name: "Jane Smith", location: { township: 2, range: 1, section: 1 } },
+
+];
+
+//event listener for bottom section and to highlight grid. 
+document.getElementById("searchButton").addEventListener("click", function() {
+
+  const search = document.getElementById("nameInput").value.toLowerCase()
+  
+  //filters through data and grabs anyone with the name you input. Creates an array that holds the persons data
+  let recordName = records.filter(person => person.name.toLowerCase() == search) 
+  
+  
+  //loops through the array made by the filter() method and grabs individual properties. 
+  //will be used to show names(grantee and grantor), dates etc. 
+  //will need divs for the bottom section 
+  recordName.forEach(record=> {
+    let township = record.location.township
+    let range = record.location.range
+    let section = record.location.section
+    let name = record.name 
+  
+    
+  });
+  
+  })
