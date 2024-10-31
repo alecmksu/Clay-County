8b0c358040e4f4b1464462e0a53bcb8a791da256
sighmn <simon19@ksu.edu>
2024-10-31 14:01:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,65 +14,68 @@ document.getElementById('filter').addEventListener('click', function() {
   const records = [
     { name: "John Doe", location: { township: 6, range: 1, section: 3 } },
     { name: "John Doe", location: { township: 6, range: 4, section: 5 } },
-    { name: "Jane Smith", location: { township: 2, range: 1, section: 1 } },
+    { name: "Jane Smith", location: { township: 7, range: 1, section: 1 } },
 
 ];
 
 
-      const outsideGrid = document.querySelector('.outsideGrid')
-      const insideGrid = document.querySelector('.insideGrid');
+    const outsideGrid = document.querySelector('.outsideGrid')
+    const insideGrid = document.querySelector('.insideGrid');
 
-      function gridMap() {
+    function gridMap() {
 
-
-      for(let row = 6; row < 11; row++) {
-          for(let column = 1; column < 5 ; column ++) {
+      //creates the 4x5 outer grid(township and range)
+      for(let township = 6; township < 11; township++) {
+          for(let range = 1; range < 5 ; range ++) {
           const cell = document.createElement('div');
           cell.classList.add('cell');
-          cell.id = `OuterCell-Row${row}-column${column}`
+          cell.id = `OuterCell-Row${township}-column${range}`
           outsideGrid.appendChild(cell);
 
-      for(let j = 1; j < 37; j++) {
+      //creates the 6x6 inner grid(sections)
+      for(let section = 1; section < 37; section++) {
           const cellInside = document.createElement('div')
           cellInside.classList.add("innerCell")
-          cellInside.id = `InnerCell-TownShip${row}-Range${column}-Section${j}`
+          cellInside.id = `InnerCell-TownShip${township}-Range${range}-Section${section}`
           cell.appendChild(cellInside)
       }
     }
   }
 }
-  
-gridMap(); 
 
 //event listener for bottom section and to highlight grid. 
 document.getElementById("searchButton").addEventListener("click", function() {
 
-const search = document.getElementById("nameInput").value.toLowerCase()
+//clears the highlights when you want another person.
+let clearTheGrids = document.querySelectorAll(".highlight")
+clearTheGrids.forEach(grid => {
+  grid.classList.remove("highlight"); 
+})
+
+const search = document.getElementById("nameInput").value.toLowerCase(); 
 
 //filters through data and grabs anyone with the name you input. Creates an array that holds the persons data
-let recordName = records.filter(person => person.name.toLowerCase() == search) 
-
+let recordName = records.filter(person => person.name.toLowerCase() == search); 
 
 //loops through the array made by the filter() method and grabs individual properties. 
 //will be used to show names(grantee and grantor), dates etc. 
 //will need divs for the bottom section 
 recordName.forEach(record => {
-  let row = record.location.township
-  let column = record.location.range
-  let j = record.location.section
-  console.log(row, column, j)
-  //grabs the id based on the locations
-  const targetId = `InnerCell-TownShip${row}-Range${column}-Section${j}`
-  console.log(targetId)
-  const targetSection = document.getElementById(targetId);
+  let township = record.location.township
+  let range = record.location.range
+  let section = record.location.section
+  let name = record.name
 
+  //grabs the id based on the locations
+  const targetId = `InnerCell-TownShip${township}-Range${range}-Section${section}`
+  const targetSection = document.getElementById(targetId);
 //adds highlights depending on the location of the record
-if (targetSection) {
+  if(targetSection) {
     targetSection.classList.add('highlight'); 
-} else {
-  alert('');
-}
+  } else {
+    alert('Not A Valid Name!');
+  }
 
 });
-
 })
+gridMap(); 
